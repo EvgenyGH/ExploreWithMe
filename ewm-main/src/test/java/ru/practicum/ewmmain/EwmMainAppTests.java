7e24cc647769898f18d41151aa712bab7a06d0ee
Evgeny S <j3000@bk.ru>
2022-09-20 14:39:59 +0300

    feat: initialize module ewm-main

---
+++ ewm-main/src/test/java/ru/practicum/ewmmain/EwmMainAppTests.java
@@ -0,0 +1,13 @@
+package ru.practicum.ewmmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwmMainAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
