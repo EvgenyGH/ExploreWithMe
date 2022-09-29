c5c9ec00499d935ab573cbc0e08ffabf7a98788b
Evgeny S <j3000@bk.ru>
2022-09-29 20:33:48 +0300

    feat: Add docker containers and docker-compose

--- ewm-main/src/test/java/ru/practicum/ewmmain/EwmMainAppTests.java
+++ ewm-main/src/test/java/ru/practicum/ewmmain/EwmMainAppTests.java
@@ -1,9 +1,7 @@
 package ru.practicum.ewmmain;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class EwmMainAppTests {
 
     @Test
--- ewm-stats/src/test/java/ru/practicum/ewmstats/EwmStatsAppTests.java
+++ ewm-stats/src/test/java/ru/practicum/ewmstats/EwmStatsAppTests.java
@@ -1,9 +1,7 @@
 package ru.practicum.ewmstats;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class EwmStatsAppTests {
 
     @Test
