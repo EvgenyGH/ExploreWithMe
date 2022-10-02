83ddf9e28b973b8928a3946dd7e4f5db398dd8d6
Evgeny S <j3000@bk.ru>
2022-10-02 20:57:40 +0300

    refactor: codestyle.

--- ewm-main/src/main/java/ru/practicum/ewmmain/model/event/location/dto/LocationDto.java
+++ ewm-main/src/main/java/ru/practicum/ewmmain/model/event/location/dto/LocationDto.java
@@ -6,8 +6,10 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
+
 /**
  * Dto класса географических координат {@link ru.practicum.ewmmain.model.event.location.Location}.
+ *
  * @author Evgeny S
  * @see ru.practicum.ewmmain.model.event.location.Location
  */
